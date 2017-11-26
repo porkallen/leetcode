c6200ae58a252fa2b089c605129b601870331b14
porkallen <allenms886.com>
2017-11-26 04:35:57 -0800

    backtracking

---
+++ src/leetcode/BackTrackingPractice.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BackTrackingPractice {
+	public int[] checked = new int[100];
+	public List<String> strPool = new LinkedList<String>();
+	public void stringOrder(String str) {
+		this.backTracking(str,str.length());
+	}
+	public static int backTracking(String str, int sz) {
+		
+		for(int i = 0 ; i < sz; i++) {
+			if()
+			
+			
+		}
+		
+		
+		return 0;
+	}
+
+}
