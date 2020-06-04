85081334b9cbc63f53234b58168dc02583bcfed5
Allen Chen <allen@occipital.com>
2020-06-03 22:03:06 -0700

    Added q102

--- C++/Source/Tree/102.cpp
+++ C++/Source/Tree/102.cpp
@@ -3,34 +3,65 @@
 class Solution {
 public:
 
-    void dfs(TreeNode* node ,vector<vector<int>> ret, int n){
+    void bfs(TreeNode* node ,vector<vector<int>> &ret){
+        deque<TreeNode *> dq;
+        int level = 0;
         if(!node)
             return;
 
+        dq.push_back(node);
+        while (!dq.empty())
+        {
+            ret.push_back({});
+            int num = dq.size();
+            for (int i = 0; i < num; i++)
+            {
+                TreeNode *t = dq.front();
+                dq.pop_front();
+                ret[level].push_back(t->val);
+                if (t->left)
+                    dq.push_back(t->left);
+                if(t->right)
+                    dq.push_back(t->right);
+            }
+            level++;
+        }
+    }
+
+    void dfs(TreeNode* node ,vector<vector<int>> &ret, int n){
+        if(!node)
+            return;
+        if(ret.size() <= n)
+            ret.push_back({});
         ret[n].push_back(node->val);
         dfs(node->left, ret, n + 1);
         dfs(node->right, ret, n + 1);
     }
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> ret;
-        dfs(root, ret, 0);
+        //dfs(root, ret, 0);
+        bfs(root, ret);
 
-        cout << "ret empty ?: " << ret.empty() << "\n";
-        for (auto it : ret)
-        {
-            for(auto it2 : it){
-                cout << "val:" << it2 << "\n";
+        for (int i = 0; i < ret.size(); i++){
+            cout << "ith:" << i << "\n";
+            for (auto it : ret[i])
+            {
+                cout << it << " ";
             }
+            cout << "\n";
         }
-        return ret;
+            return ret;
     }
 };
 
 int main(){
     Solution s;
-    TreeNode root(10);
-    root.left = new TreeNode(5);
-    root.right = new TreeNode(12);
+    TreeNode root(3);
+    root.left = new TreeNode(9);
+    root.right = new TreeNode(20);
+    root.right->left = new TreeNode(15);
+    root.right->right = new TreeNode(7);
     s.levelOrder(&root);
+
     return 0;
 }
